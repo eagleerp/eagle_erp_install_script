ec67c706611f6421e33fd755c65c610614265649
ShaheenHossain <rapidgrps@princegroup-bd.com>
2023-02-28 16:26:53 +0600

    Update eagle-install-1221.sh

--- eagle-install-1221.sh
+++ eagle-install-1221.sh
@@ -39,7 +39,7 @@ echo -e "\n--- Installing Python 3 + pip3 --"
 sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi wkhtmltopdf -y
 
 echo -e "\n---- Install python packages/requirements ----"
-sudo pip3 install -r https://github.com/ShaheenHossain/eagle12c1.2/master/requirements.txt
+sudo pip3 install -r https://raw.githubusercontent.com/ShaheenHossain/eagle12c1.2/master/requirements.txt
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm
