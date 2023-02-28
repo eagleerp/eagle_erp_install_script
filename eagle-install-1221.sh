bfc760e0835e67d7f15e811bbb4c59a189cc0ab0
ShaheenHossain <rapidgrps@princegroup-bd.com>
2023-02-28 16:32:49 +0600

    Update eagle-install-1221.sh

--- eagle-install-1221.sh
+++ eagle-install-1221.sh
@@ -36,7 +36,7 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 # Install Dependencies
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi wkhtmltopdf -y
+sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less gdebi wkhtmltopdf -y
 
 echo -e "\n---- Install python packages/requirements ----"
 sudo pip3 install -r https://raw.githubusercontent.com/ShaheenHossain/eagle12c1.2/master/requirements.txt
