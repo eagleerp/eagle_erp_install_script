e248ff9bc73786ebe81a1222a3739c997a7ec2ba
Eagle ERP <136437751+eagleerp@users.noreply.github.com>
2023-06-13 16:39:20 +0600

    Update eagle-install-1221.sh

--- eagle-install-1221.sh
+++ eagle-install-1221.sh
@@ -39,8 +39,7 @@ echo -e "\n--- Installing Python 3 + pip3 --"
 sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less gdebi wkhtmltopdf -y
 
 echo -e "\n---- Install python packages/requirements ----"
-
-sudo pip3 install -r https://github.com/eagleerp/eagle_erp_1.2c/master/requirements.txt
+sudo pip3 install -r https://raw.githubusercontent.com/eagle_erp_1.2c/master/requirements.txt
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm
