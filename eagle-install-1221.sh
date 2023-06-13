d69d56af55e385d3c50a0e66f47f5dc2473f47ab
Eagle ERP <136437751+eagleerp@users.noreply.github.com>
2023-06-13 21:05:00 +0600

    Update eagle-install-1221.sh

--- eagle-install-1221.sh
+++ eagle-install-1221.sh
@@ -36,14 +36,10 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 # Install Dependencies
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
-#sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less gdebi wkhtmltopdf -y
-sudo apt-get install python-dev python3-dev libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev 
+sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxml2-dev libxslt1-dev zlib1g-dev libzip-dev libldap2-dev libsasl2-dev libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev libblas-dev libatlas-base-dev python3-setuptools node-less gdebi wkhtmltopdf -y
 
 echo -e "\n---- Install python packages/requirements ----"
 sudo pip3 install -r https://raw.githubusercontent.com/eagleerp/eagle_erp_1.2c/master/requirements.txt
-
-#sudo pip3 install -r https://raw.githubusercontent.com/ShaheenHossain/requirements_12/master/requirements.txt
-
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm
